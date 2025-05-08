9eb6f3470b7af177843ec9ba5b4d2b488e11c7d0
Shenoda <shenodamakramibrahim@gmail.com>
2025-05-08 22:00:23 +0300

    .

--- models/Product.js
+++ models/Product.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   img: { type: String, required: true },
   name: { type: String, required: true },
-  numOfRating: { type: Number, required: true },
+  numOfRating: { type: Number, required: false, default: 10 },
   price: { type: Number, required: true },
-  rating: { type: Number, required: true },
+  rating: { type: Number, required: false, default: 4 },
   tag: { type: String, required: true },
 });
 
